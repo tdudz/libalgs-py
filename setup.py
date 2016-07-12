3420d2614acd90290a1b5234de0f38b30f407966
Tom Dudzik <tdudz97@gmail.com>
2016-07-12 03:02:58 -0400

    add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='libalgs-py',
+      version='0.1',
+      packages=['libalgs-py'],
+      license='MIT',
+      url='https://github.com/tdudz/libalgs-py',
+      author='Tom Dudzik',
+      description='A library of generic algorithms and data structures in python, written from scratch',
+      keywords='library python algorithms data structures')
